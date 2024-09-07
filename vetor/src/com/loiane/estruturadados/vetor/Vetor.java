7711f0754ccce81adae774166116232dfad0c79c
Davi <obrigatoriopcional@gmail.com>
2024-09-07 16:18:13 -0300

    mretornar tamanho e imprimir elementos do vetor com o étodo toString

--- vetor/src/com/loiane/estruturadados/vetor/Vetor.java
+++ vetor/src/com/loiane/estruturadados/vetor/Vetor.java
@@ -1,5 +1,7 @@
 package com.loiane.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
     private String[] elementos;
     private int tamanho;
@@ -42,5 +44,29 @@ public class Vetor {
             return true;
         }
         return false;
+    }
+
+    public int tamanho(){
+        return this.tamanho;
+    }
+
+    public String toString(){
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("[");
+
+        for(int i = 0; i < this.tamanho - 1; i++){
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+        if(this.tamanho > 0){
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
     }
 }
